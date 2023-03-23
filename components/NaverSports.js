63e8decae78e874e7cf8834b143c3af509aa284a
sonhun99 <57802358+sonhun99@users.noreply.github.com>
2023-03-23 18:04:24 +0900

    NaverSports

---
+++ components/NaverSports.js
@@ -0,0 +1,22 @@
+const NaverSports = () => {
+    const cheerio = require('cheerio');
+    const axios = require('axios');
+
+    axios.get('https://m.sports.naver.com/kbaseball/video?sort=date&date=2023-03-21&tab=date&themeType=type&themeCode=2')
+        .then((response) => {
+            const $ = cheerio.load(response.data);
+            const elements = $('a.VideoList_link_video__TlHcC');
+            console.log(elements);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    return (
+        <div>
+            <h1>NaverSports</h1>
+            <h2>{elements}</h2>
+        </div>
+    );
+};
+
+export default NaverSports;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Clock from '@components/Clock'
+import NaverSports from '@components/NaverSports'
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
 
       <Footer />
       <Clock />
+      <NaverSports />
     </div>
   )
 }
